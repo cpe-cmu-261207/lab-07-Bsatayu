5af226c956fce7ffd40eef707170d34f3364721a
Bsatayu <satayu_s@cmu.ac.th>
2021-02-02 12:43:01 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import {useEffect} from 'react';
 import axios from 'axios'
-import Link from 'next/link';
 const baseURL = 'https://dummyapi.io/data/api'
 const Home = () => {
   return (
-    <body>
-          <table style = {{width:" 100%;", padding: "50px", margin:"5px"}}>
-            <tr align="center">
-                      <td><Link href={"/homepage"}>Home</Link></td>
-                      <td><Link href={"/gallery"}>Gallery</Link></td>
-                      <td><Link href={"/contact"}>Contact</Link></td>
-                      <td><Link href={"/gpa"}>GPA Calculator</Link></td>
-                      <td><Link href={"/post"}>Post</Link></td>
-                      </tr>
-          </table>
-      </body>
+    <>
+      <h1>This is home page</h1>
+      <link href="post">
+        <button>goto post page</button>
+      </link>
+      <link href="GPA Cal">
+        <button>GPA Calculator</button>
+      </link>
+      <link href="Home">
+        <button>Home</button>
+      </link>
+      </>
   )
 }
 
